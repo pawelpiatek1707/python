762cc781b71982195c5247f5eaed34331a524bb2
Pawel <pawelpiatek98@gmail.com>
2021-11-07 15:04:00 +0100

    created zad_1

---
+++ zad_1.py
@@ -0,0 +1,8 @@
+names_list: list = ["Adam", "Anna", "Kamil", "Tomasz", "Krystian"]
+
+def print_names(names: list) -> None:
+    for name in names_list:
+        print(name)
+
+
+print_names(names_list)
