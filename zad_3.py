7dc7d24fc35219825437c50a11b9f938432a1ad9
Pawel <pawelpiatek98@gmail.com>
2021-11-07 15:35:27 +0100

    added zad_4

--- zad_3.py
+++ zad_3.py
@@ -4,8 +4,8 @@ def return_even(numbers: list) -> list:
     even_numbers = list()
 
     for num in range(0, len(numbers)):
-        if num % 2 == 0:
-            even_numbers.append(num)
+        if numbers[num] % 2 == 0:
+            even_numbers.append(numbers[num])
 
     return even_numbers
 
---
+++ zad_4.py
@@ -0,0 +1,13 @@
+num_list: list = [12,24,13,54,32,56,43,77,65,44]
+
+def return_even_index(numbers: list) -> list:
+    returned_elements = list()
+
+    for index in range(0, len(numbers)):
+        if index % 2 == 0:
+            returned_elements.append(numbers[index])
+
+    return returned_elements
+
+
+print(return_even_index(num_list))
