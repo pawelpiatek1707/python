ba9351b99a02c6d9e73fb63ff49fea4d900e3eb1
Pawel <pawelpiatek98@gmail.com>
2021-11-21 16:42:25 +0100

    fixed zadanie3

--- zadanie3.py
+++ zadanie3.py
@@ -1,8 +1,9 @@
-def check_even(num: int):
+def check_even(num: int)-> bool:
     return num % 2 == 0
 
 is_even = check_even(4)
-is_even2 = check_even(5)
 
-print(is_even)
-print(is_even2)
+if is_even:
+    print('liczba jest parzysta')
+else:
+    print('liczba jest nieparzysta')
