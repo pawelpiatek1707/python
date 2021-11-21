b03f3d8dbb968514b87ee1ec48874f1e11fd896e
Pawel <pawelpiatek98@gmail.com>
2021-11-21 16:36:29 +0100

    finished zadanie2

---
+++ zadanie1.py
@@ -0,0 +1,6 @@
+def print_name(name: str, surname: str) -> str:
+    return f"Cześć {name} {surname}"
+
+greeting = print_name('Jan', 'Nowak')
+
+print(greeting)
---
+++ zadanie2.py
@@ -0,0 +1,4 @@
+def multiply(num1: int, num2: int)-> int:
+    return num1 * num2
+
+print(multiply(2,4))
