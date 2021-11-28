bf2e8080e7ff705061815bdbeb642d1e632cf6be
Pawel <pawelpiatek98@gmail.com>
2021-11-28 15:52:38 +0100

    formated code

--- zad2.py
+++ zad2.py
@@ -2,7 +2,14 @@ from zad1 import Student
 
 
 class Library:
-    def __init__(self, city: str, street: str, zip_code: str, open_hours: str, phone: str) -> None:
+    def __init__(
+        self,
+        city: str,
+        street: str,
+        zip_code: str,
+        open_hours: str,
+        phone: str
+    ) -> None:
         self.city = city
         self.street = street
         self.zip_code = zip_code
@@ -10,21 +17,42 @@ class Library:
         self.phone = phone
 
     def __str__(self) -> str:
-        return f"Biblioteka w miescie: {self.city}, przy ulicy {self.zip_code} otwarta w godzianch {self.open_hours}"
+        return f"""Biblioteka w miescie:
+                {self.city}, przy ulicy {self.zip_code}
+                otwarta w godzianch {self.open_hours}"""
+
 
 class Order:
-    def __init__(self, employee: str, student: str, books: list, order_date: str) -> None:
+    def __init__(
+        self,
+        employee: str,
+        student: str,
+        books: list,
+        order_date: str
+    ) -> None:
         self.employee = employee
         self.student = student
         self.books = books
         self.order_date = order_date
 
     def __str__(self) -> str:
-         return f"Zamowienie zamowione dnia: {self.order_date}, przez {self.student}, zrealizowane przez {self.employee}"
+        return f"""Zamowienie zamowione dnia:
+            {self.order_date}, przez {self.student},
+            zrealizowane przez {self.employee}"""
 
 
 class Employee:
-    def __init__(self, first_name: str, last_name: str, hire_date: str, birth_date: str, city: str, street: str, zip_code: str, phone: str) -> None:
+    def __init__(
+        self,
+        first_name: str,
+        last_name: str,
+        hire_date: str,
+        birth_date: str,
+        city: str,
+        street: str,
+        zip_code: str,
+        phone: str
+    ) -> None:
         self.first_name = first_name
         self.last_name = last_name
         self.hire_date = hire_date
@@ -35,10 +63,20 @@ class Employee:
         self.phone = phone
 
     def __str__(self) -> str:
-        return f"Pracownik {self.first_name} {self.last_name} urodzony {self.birth_date} zatrudniony {self.hire_date}"
+        return f"""Pracownik
+            {self.first_name} {self.last_name} urodzony
+            {self.birth_date} zatrudniony {self.hire_date}"""
+
 
 class Book:
-    def __init__(self, library: str, publication_date: str, author_name: str, author_surname: str, number_of_pages: int) -> None:
+    def __init__(
+        self,
+        library: str,
+        publication_date: str,
+        author_name: str,
+        author_surname: str,
+        number_of_pages: int
+    ) -> None:
         self.library = library
         self.publication_date = publication_date
         self.author_name = author_name
@@ -46,7 +84,10 @@ class Book:
         self.number_of_pages = number_of_pages
 
     def __str__(self) -> str:
-        return f"Ksiazka z biblioteki {self.library}. Autor ksiazki: {self.author_name} {self.author_surname}"
+        return f"""Ksiazka z biblioteki
+            {self.library}. Autor ksiazki:
+            {self.author_name} {self.author_surname}"""
+
 
 library1 = Library('Katowice', 'Mikołowksa', '33-123', '8-16', '653 566 762')
 library1 = Library('Kraków', 'Długa', '23-423', '9-17', '453 567 654')
@@ -57,9 +98,12 @@ book3 = Book('Biblioteka Katowice', '02,10,1984', 'Stefan', 'Kowalkis', 320)
 book4 = Book('Biblioteka Kraków', '07,10,1991', 'Alicja', 'Kot', 320)
 book5 = Book('Biblioteka Katowice', '01,10,2012', 'Krzysztof', 'Polak', 320)
 
-emp1 = Employee("Jan", "Nowak", "03.11.2018", "10.11.1990", "Katowice", "Dluga", "43-112", "123456789")
-emp1 = Employee("Jan", "Kowalski", "03.09.2016", "10.11.1995", "Katowice", "Szeroka", "43-112", "987654321")
-emp1 = Employee("Kamil", "Nowak", "03.12.2015", "10.11.1998", "Katowice", "Głęboka", "43-112", "654123890")
+emp1 = Employee("Jan", "Nowak", "03.11.2018", "10.11.1990",
+                "Katowice", "Dluga", "43-112", "123456789")
+emp1 = Employee("Jan", "Kowalski", "03.09.2016", "10.11.1995",
+                "Katowice", "Szeroka", "43-112", "987654321")
+emp1 = Employee("Kamil", "Nowak", "03.12.2015", "10.11.1998",
+                "Katowice", "Głęboka", "43-112", "654123890")
 
 stud1 = Student("Jan", "Kowalski")
 stud2 = Student("Wojciech", "Nowak")
